af30603038e4bddafe3aebfe4d124d46688db679
KPG143 <34210285+kipkat@users.noreply.github.com>
2018-04-07 15:41:00 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: null
+ * Date: 11.11.2017
+ * Time: 13:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Kippapi
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.Black;
+			this.textBox1.ForeColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(607, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "website";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Black;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(625, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 20);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Get";
+			this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.Black;
+			this.richTextBox1.ForeColor = System.Drawing.Color.White;
+			this.richTextBox1.Location = new System.Drawing.Point(12, 38);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(688, 417);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "api";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.BackColor = System.Drawing.Color.Black;
+			this.richTextBox2.ForeColor = System.Drawing.Color.White;
+			this.richTextBox2.Location = new System.Drawing.Point(12, 461);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.ReadOnly = true;
+			this.richTextBox2.Size = new System.Drawing.Size(688, 104);
+			this.richTextBox2.TabIndex = 3;
+			this.richTextBox2.Text = "elements";
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.Black;
+			this.textBox2.ForeColor = System.Drawing.Color.Azure;
+			this.textBox2.Location = new System.Drawing.Point(12, 571);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(584, 20);
+			this.textBox2.TabIndex = 4;
+			this.textBox2.Text = "element";
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Black;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(602, 571);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(98, 20);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Check";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.checkelement);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Desktop;
+			this.ClientSize = new System.Drawing.Size(712, 597);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Opacity = 0.95D;
+			this.Text = "Kippapi";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Kippapi
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			button2.Text="Check";
+			try
+			{
+				richTextBox2.Text="";
+				if (!textBox1.Text.Contains("https://"))
+				{
+					if (!textBox1.Text.Contains("http://")) textBox1.Text="http://"+textBox1.Text;
+				}
+				string api = new System.Net.WebClient().DownloadString(textBox1.Text);
+				richTextBox1.Text=api;
+				if (richTextBox1.Text.Contains("true"))
+				{
+					richTextBox2.Text+="True boolean detected. ";
+				}
+				if (richTextBox1.Text.Contains("\"content\""))
+				{
+					richTextBox2.Text+="Content boxes. ";
+				}
+				if (richTextBox1.Text.Contains("style"))
+				{
+					richTextBox2.Text+="Style found. ";
+				}
+				if (richTextBox1.Text.Contains("<--"))
+				{
+					richTextBox2.Text+="Comments. ";
+				}
+				if (richTextBox1.Text.Contains("type=\"password\""))
+				{
+					richTextBox2.Text+="Passwords. ";
+				}
+				if (richTextBox1.Text.Contains("detector"))
+				{
+					richTextBox2.Text+="Detector found. ";
+				}
+				if (richTextBox1.Text.Contains("language=\"javascript\""))
+				{
+					richTextBox2.Text+="Javascript scripts. ";
+				}
+				if (richTextBox1.Text.Contains("<script>"))
+				{
+					richTextBox2.Text+="Scripts. ";
+				}
+				if (richTextBox1.Text.Contains("alert("))
+				{
+					richTextBox2.Text+="Alert boxes. ";
+				}
+				if (richTextBox1.Text.Contains("ads"))
+				{
+					richTextBox2.Text+="Ads detected! ";
+				}
+				if (richTextBox1.Text.Contains("youtube"))
+				{
+					richTextBox2.Text+="Youtube video. ";
+				}
+				if (richTextBox1.Text.Contains("google"))
+				{
+					richTextBox2.Text+="Google services. ";
+				}
+				if (richTextBox1.Text.Contains("service"))
+				{
+					richTextBox2.Text+="Services. ";
+				}
+				if (richTextBox1.Text.Contains(".onion"))
+				{
+					richTextBox2.Text+="Deep-web links. ";
+				}
+				if (richTextBox1.Text.Contains("simulator"))
+				{
+					richTextBox2.Text+="Simulation found. ";
+				}
+				if (richTextBox1.Text.Contains("secret"))
+				{
+					richTextBox2.Text+="Secrets found. ";
+				}
+				if (richTextBox1.Text.Contains("wiki"))
+				{
+					richTextBox2.Text+="Wiki services. ";
+				}
+				if (richTextBox1.Text.Contains("adb"))
+				{
+					richTextBox2.Text+="Adblocker services. ";
+				}
+				if (richTextBox1.Text.Contains(".exe")){richTextBox2.Text+="Exe files found. ";}
+				if (richTextBox1.Text.Contains("../")){richTextBox2.Text+="Reverse directory found. ";}
+				if (richTextBox1.Text.Contains("0x")){richTextBox2.Text+="Hex values found. ";}
+				if (richTextBox1.Text.Contains("test")){richTextBox2.Text+="Testing content detected. ";}
+				if (richTextBox1.Text.Contains("window")){richTextBox2.Text+="Window changer detected! ";}
+				if (richTextBox1.Text.Contains("color")){richTextBox2.Text+="Color values found. ";}
+				if (richTextBox1.Text.Contains("log")){richTextBox2.Text+="Logs found. ";}
+				if (richTextBox1.Text.Contains("666")){richTextBox2.Text+="Satanic messages detected! ";}
+				if (richTextBox1.Text.Contains("satan")){richTextBox2.Text+="Satan detected! ";}
+				if (richTextBox1.Text.Contains("email")){richTextBox2.Text+="Email field found. ";}
+				if (richTextBox1.Text.Contains("mail")){richTextBox2.Text+="Mail field found. ";}
+				if (richTextBox1.Text.Contains("api")){richTextBox2.Text+="Api detected! ";}
+				if (richTextBox1.Text.Contains("parameter")){richTextBox2.Text+="Parameters detected! ";}
+				if (richTextBox1.Text.Contains("argument")){richTextBox2.Text+="Arguments detected! ";}
+				if (richTextBox1.Text.Contains("game")){richTextBox2.Text+="Games detected! ";}
+				if (richTextBox1.Text.Contains("comment")){richTextBox2.Text+="Public user comments detected! ";}
+				if (richTextBox1.Text.Contains("xss")){richTextBox2.Text+="XSS fields! ";}
+				if (richTextBox1.Text.Contains("sql")){richTextBox2.Text+="SQL actions detected! ";}
+				if (richTextBox1.Text.Contains("database")){richTextBox2.Text+="Database detected! ";}
+				if (richTextBox1.Text.Contains("db")){richTextBox2.Text+="Hidden database detected! ";}
+				if (richTextBox1.Text.Contains("opyright")){richTextBox2.Text+="Copyright detected! ";}
+				if (richTextBox1.Text.Contains("debug")){richTextBox2.Text+="Debug actions found. ";}
+				if (richTextBox1.Text.Contains("button")){richTextBox2.Text+="Buttons found. ";}
+				if (richTextBox1.Text.Contains("toggle")){richTextBox2.Text+="Toggle elements found. ";}
+				if (richTextBox1.Text.Contains("www")){richTextBox2.Text+="WWW Server found. ";}
+				if (richTextBox1.Text.Contains("music")){richTextBox2.Text+="Music elements found. ";}
+				if (richTextBox1.Text.Contains("404")){richTextBox2.Text+="Server not found value. ";}
+				if (richTextBox1.Text.Contains("cookie")){richTextBox2.Text+="Cookies found. ";}
+				if (richTextBox1.Text.Contains("like")){richTextBox2.Text+="Like-Counter detected. ";}
+				if (richTextBox1.Text.Contains("dislike")){richTextBox2.Text+="Dislike-Counter detected. ";}
+				if (richTextBox1.Text.Contains("counter")){richTextBox2.Text+="Counter detected. ";}
+				if (richTextBox1.Text.Contains("var ")){richTextBox2.Text+="Unexpected variable definitions. ";}
+				if (richTextBox1.Text.Contains("type")){richTextBox2.Text+="Types elements found. ";}
+				if (richTextBox1.Text.Contains("javascript:")){richTextBox2.Text+="Forced javascript conversion detected! ";}
+				if (richTextBox1.Text.Contains("analytics")){richTextBox2.Text+="Analytics elements found!!! ";}
+				if (richTextBox1.Text.Contains("login")){richTextBox2.Text+="Login detected!!! ";}
+				if (richTextBox1.Text.Contains("id=")){richTextBox2.Text+="ID export found!!! ";}
+				if (richTextBox1.Text.Contains("expert")){richTextBox2.Text+="Solution found!!! ";}
+				if (richTextBox1.Text.Contains("algorithm")){richTextBox2.Text+="Algorithms found!!! ";}
+				if (richTextBox1.Text.Contains("hacked")){richTextBox2.Text+="DEFACED PAGE SOURCE!!! ";}
+				if (richTextBox1.Text.Contains("pwd")){richTextBox2.Text+="Hidden password found!!! ";}
+				if (richTextBox1.Text.Contains("()")){richTextBox2.Text+="Non-argument functions detected!!! ";}
+				if (richTextBox1.Text.Contains("time")){richTextBox2.Text+="Time set detected!!! ";}
+				if (richTextBox1.Text.Contains("trace")){richTextBox2.Text+="Tracing methods found!!! ";}
+				if (richTextBox1.Text.Contains("secured")){richTextBox2.Text+="Secured elements found!!! ";}
+				if (richTextBox1.Text.Contains("\"\"")){richTextBox2.Text+="Null string detected! ";}
+				if (richTextBox1.Text.Contains("tor")){richTextBox2.Text+="Tor browser detector found! ";}
+				if (richTextBox1.Text.Contains("anonymous")){richTextBox2.Text+="Anonymous detector found!!! ";}
+				if (richTextBox1.Text.Contains("directx")){richTextBox2.Text+="Directx scanner found!!! ";}
+				if (richTextBox1.Text.Contains("webgl")){richTextBox2.Text+="WebGL scripts found!!! ";}
+				if (richTextBox1.Text.Contains("map")){richTextBox2.Text+="Maps found!!! ";}
+				if (richTextBox1.Text.Contains("set")){richTextBox2.Text+="Settings found!!! ";}
+				if (richTextBox1.Text.Contains("/*")){richTextBox2.Text+="Style comment! ";}
+				if (richTextBox1.Text.Contains("<!--")){richTextBox2.Text+="Comment1 found!!! ";}
+				if (richTextBox1.Text.Contains("<--")){richTextBox2.Text+="Comment2 found!!! ";}
+				if (richTextBox1.Text.Contains("42")){richTextBox2.Text+="Life number found!!! ";}
+				if (richTextBox1.Text.Contains("00")){richTextBox2.Text+="Double null found!!! ";}
+				if (richTextBox1.Text.Contains("useless")){richTextBox2.Text+="Useless elements found!!! ";}
+				if (richTextBox1.Text.Contains("\">")){richTextBox2.Text+="XSS escape methods found!!! ";}
+				if (richTextBox1.Text.Contains("market")){richTextBox2.Text+="Market found!!! ";}
+			}
+			catch
+			{
+				richTextBox1.Text="Error";
+				richTextBox2.Text="Error";
+			}
+		}
+		void checkelement(object sender, System.EventArgs e)
+		{
+			if (richTextBox1.Text.Contains(textBox2.Text)){
+				button2.Text="True. Check";
+			}
+			else button2.Text="False. Check";
+		}
+	}
+}
